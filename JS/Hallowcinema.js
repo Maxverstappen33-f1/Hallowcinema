c81b333f2786e2cd17c26d92e1a6164adc27e205
Maxverstappen33-f1 <fracappuccino347@gmail.com>
2024-10-26 21:30:49 +0200

    Update Hallowcinema.js

--- JS/Hallowcinema.js
+++ JS/Hallowcinema.js
@@ -3,7 +3,7 @@ function validateForm() {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('errorMessage');
 
-    if (username === 'Admin' && password === '31/10/24') {
+    if (username === 'admin' && password === '311024') {
         document.body.innerHTML = `
             <div class="container">
                 <h1 class="success-message">Login success</h1>
